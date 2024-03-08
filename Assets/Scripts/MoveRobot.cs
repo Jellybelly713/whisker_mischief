f80dacf67c6895db12422dd9985cc43414b5b865
Jayne Davies <jaynedavies@cmail.carleton.ca>
2024-03-07 19:45:47 -0500

    Adding Robot Animation
    It is clipping in the floor, not sure why.
    I'll try to fix it later.

--- Assets/Scripts/MoveRobot.cs
+++ Assets/Scripts/MoveRobot.cs
@@ -8,12 +8,30 @@ public class MoveRobot : MonoBehaviour
     public Transform[] BotPoints;
     NavMeshAgent navAgent;
     int currentBotPointIndex = 0;
+    Animator animator;
+    bool isNotIdle = true;
 
     // Start is called before the first frame update
     void Start()
     {
         navAgent = GetComponent<NavMeshAgent>();
         navAgent.SetDestination(BotPoints[currentBotPointIndex].position);
+        
+        animator = GetComponent<Animator>();
+
+    }
+
+    IEnumerator IdleDelayCoroutine()
+    {
+        //set speed to 0
+        animator.SetFloat("SpeedBot", 0f);
+
+        //wait 3 seconds
+        yield return new WaitForSeconds(3);
+        // set speed to 1
+        animator.SetFloat("SpeedBot", 1.0f);
+        isNotIdle = false;
+
     }
 
     // Update is called once per frame
@@ -26,7 +44,11 @@ public class MoveRobot : MonoBehaviour
         {
             currentBotPointIndex = (currentBotPointIndex + 1) % BotPoints.Length;
             navAgent.SetDestination(BotPoints[currentBotPointIndex].position);
-
+           // while (isNotIdle == true)
+           // {
+           //     Debug.Log("in while loop");
+           //         StartCoroutine(IdleDelayCoroutine());
+           // }
         }
     }
 }
