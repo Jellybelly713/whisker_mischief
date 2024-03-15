c29bf9390f3bd3a4639c3106181d9c15abc24cec
Jayne Davies <jaynedavies@cmail.carleton.ca>
2024-03-15 10:49:48 -0400

    Robot movement
    Semiworking

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -2,13 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Movement : MonoBehaviour
 {
     public float speed = 5;
-    public Rigidbody rb;
-    public float forceAmount = 10;
+    public float forceAmount = 20;
     bool canJump = false;
+
+    [SerializeField] private float mouseSensitivity = 200f;
+    public Vector2 turn;
+
+
+    public Rigidbody rb;
     Animator animator;
     Vector3 movement;
     public Score changeScore;
@@ -21,40 +27,33 @@ public class Movement : MonoBehaviour
     void Update()
     {
         /////////////////// moving in 4 directions
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
 
-        movement = new Vector3(x, 0, z);
+
+        movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        /*
+        if (forceControlx > 0.2f)
+        {
+            forceControlx = 0.2f;
+        }
+
+        if (forceControlz > 0.2f)
+        {
+            forceControlz = 0.2f;
+        }
+
+        movement = new Vector3(forceControlx, 0, forceControlz);
         // this sets a magnitude so knock off controllers wont affect it 
-        movement = Vector3.ClampMagnitude(movement, 1);
+        rb.AddForce(movement, ForceMode.Impulse);
 
-        transform.Translate(movement * speed * Time.deltaTime);
+        */
 
         /////////////////////////////jumping
         if (Input.GetKeyDown(KeyCode.Space) && canJump)
         {
-            rb.AddForce(Vector3.up * forceAmount, ForceMode.Impulse);
+            rb.AddForce(new Vector3(0, forceAmount, 0), ForceMode.Impulse);
         }
 
-        ////////////////////////Rotation
-        if (Input.GetKey(KeyCode.E))
-        {
-            transform.Rotate(0, 1, 0);
-        }
-        if (Input.GetKey(KeyCode.Q))
-        {
-            transform.Rotate(0, -1, 0);
-        }
-
-
-        if (movement != Vector3.zero)
-        {
-            animator.SetFloat("Speed", 1.0f);
-        }
-        else
-        {
-            animator.SetFloat("Speed", 0.0f);
-        }
 
     }
 
@@ -67,7 +66,7 @@ public class Movement : MonoBehaviour
 
         if (other.gameObject.CompareTag("angryZone"))
         {
-            changeScore.CollisionNegPnt();        
+            changeScore.CollisionNegPnt();
         }
     }
 
@@ -79,7 +78,12 @@ public class Movement : MonoBehaviour
         }
     }
 
+    private void FixedUpdate()
+    {
+        // Cat - physics based 
+        rb.velocity = movement * speed;
 
+    }
 
     /*
     on trigger enter collider other
