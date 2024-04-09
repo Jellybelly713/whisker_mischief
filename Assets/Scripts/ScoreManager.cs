ad0aa7f24420b9e7f2592bddb7036aae1a209803
oliviascout <124936224+oliviascout@users.noreply.github.com>
2024-04-08 20:09:02 -0400

    added max chaos

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -4,20 +4,32 @@ using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
+    public static ScoreManager instance;
+
+    public int score = 0;
+
+    public GameObject maxChaosImg;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        maxChaosImg.SetActive(false);
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (score >= 20)
+        {
+            maxChaosImg.SetActive(true);
+        }
+        else
+        {
+            maxChaosImg.SetActive(false);
+        }
     }
-    public static ScoreManager instance;
-
-    public int score = 0;
 
     private void Awake()
     {
